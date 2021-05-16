5539afd064e3e20c6f899397bd31656ca7a04c0d
Thomas Liao <tliao@carboncopies.org>
2021-05-16 20:55:11 +0000

    Integrate Uvicorn Into APIServer.py

--- back-end/APIServer.py
+++ back-end/APIServer.py
@@ -10,8 +10,10 @@ Date-Created: 2021-03-03
 '''
 
 import atexit
-from random import SystemRandom
 import json
+import uvicorn
+
+from random import SystemRandom
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -24,96 +26,104 @@ from Core.Initialization.Instantiator import InstantiateZK
 from Core.Initialization.Instantiator import InstantiateLogger
 
 
+def Main(): # Create Main Function
 
-
-##############################################################################
-## NOTE: A Lowercase "m" Preceeding A Class Means It's a Main System        ##
-## NOTE: A Lowercase "s" Preceeding A Class Means It's a Subsystem          ##
-##############################################################################
-
-
-
-# Set Version Information
-Version = '0.0.7'
-Branch = 'dev' # 'dev' or 'rel'
-
-
-# Load Config #
-LoggerConfigDict = LoadLoggerConfig(ConfigFilePath = 'Config/LoggerConfig.yaml')
-DBConfigDict = LoadDatabaseConfig(ConfigFilePath = 'Config/DatabaseConfig.yaml')
-ZKConfigDict = LoadZookeeperConfig(ConfigFilePath = 'Config/ZookeeperConfig.yaml')
-
-
-# Initialize Logger #
-mLogger = InstantiateLogger(DBConfigDict, LoggerConfigDict)
-
-
-# Purges The Log Buffer On System Exit #
-@atexit.register
-def CleanLog():
-    mLogger.CleanExit()
-
-
-# Connect To Zookeeper Service #
-sZookeeper = InstantiateZK(mLogger, ZKConfigDict)
-
-
-# Define API #
-app = FastAPI()
-
-
-# Set Allowed Origins #
-
-origins = [
-    "*"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    ##############################################################################
+    ## NOTE: A Lowercase "m" Preceeding A Class Means It's a Main System        ##
+    ## NOTE: A Lowercase "s" Preceeding A Class Means It's a Subsystem          ##
+    ##############################################################################
 
 
 
-# Create A Connection zNode #
-cryptogen = SystemRandom()
-ConnectionNode = f'/BrainGenix/API/Connections/{cryptogen.randrange(38564328964397256432564372)}'
-sZookeeper.ZookeeperConnection.create(ConnectionNode, ephemeral=True)
+    # Set Version Information
+    Version = '0.0.7'
+    Branch = 'dev' # 'dev' or 'rel'
 
 
-# Define Methods In API #
-@app.get('/')
-async def root():
-    return {'message': 'test'}
-
-@app.get('/test')
-async def test():
-
-    sZookeeper.ZookeeperConnection.set(ConnectionNode, b'{"CallStack": "Version", "KeywordArgs": {}}')
-
-    while sZookeeper.ZookeeperConnection.get(ConnectionNode)[0] == b'{"CallStack": "Version", "KeywordArgs": {}}':
-        pass
+    # Load Config #
+    LoggerConfigDict = LoadLoggerConfig(ConfigFilePath = 'Config/LoggerConfig.yaml')
+    DBConfigDict = LoadDatabaseConfig(ConfigFilePath = 'Config/DatabaseConfig.yaml')
+    ZKConfigDict = LoadZookeeperConfig(ConfigFilePath = 'Config/ZookeeperConfig.yaml')
 
 
-    return json.loads(sZookeeper.ZookeeperConnection.get(ConnectionNode)[0].decode())
+    # Initialize Logger #
+    mLogger = InstantiateLogger(DBConfigDict, LoggerConfigDict)
 
 
-# Start System #
-mLogger.Log('Starting API Server')
-mLogger.Log('')
-mLogger.Log('---------------------------------------------------------------------------')
-mLogger.Log('██████╗ ██████╗  █████╗ ██╗███╗   ██╗ ██████╗ ███████╗███╗   ██╗██╗██╗  ██╗')
-mLogger.Log('██╔══██╗██╔══██╗██╔══██╗██║████╗  ██║██╔════╝ ██╔════╝████╗  ██║██║╚██╗██╔╝')
-mLogger.Log('██████╔╝██████╔╝███████║██║██╔██╗ ██║██║  ███╗█████╗  ██╔██╗ ██║██║ ╚███╔╝ ')
-mLogger.Log('██╔══██╗██╔══██╗██╔══██║██║██║╚██╗██║██║   ██║██╔══╝  ██║╚██╗██║██║ ██╔██╗ ')
-mLogger.Log('██████╔╝██║  ██║██║  ██║██║██║ ╚████║╚██████╔╝███████╗██║ ╚████║██║██╔╝ ██╗')
-mLogger.Log('╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝')
-mLogger.Log('---------------------------------------------------------------------------')
-mLogger.Log('')
-mLogger.Log('    +-----------------------------------------------------------------+')
-mLogger.Log(f'    |          Welcome To BrainGenix-APIServer Version {Version}          |')
-mLogger.Log('    +-----------------------------------------------------------------+')
-mLogger.Log('')
+    # Purges The Log Buffer On System Exit #
+    @atexit.register
+    def CleanLog():
+        mLogger.CleanExit()
+
+
+    # Connect To Zookeeper Service #
+    sZookeeper = InstantiateZK(mLogger, ZKConfigDict)
+
+
+    # Define API #
+    app = FastAPI()
+
+
+    # Set Allowed Origins #
+
+    origins = [
+        "*"
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+
+
+    # Create A Connection zNode #
+    cryptogen = SystemRandom()
+    ConnectionNode = f'/BrainGenix/API/Connections/{cryptogen.randrange(38564328964397256432564372)}'
+    sZookeeper.ZookeeperConnection.create(ConnectionNode, ephemeral=True)
+
+
+    # Define Methods In API #
+    @app.get('/')
+    async def root():
+        return {'message': 'test'}
+
+    @app.get('/test')
+    async def test():
+
+        sZookeeper.ZookeeperConnection.set(ConnectionNode, b'{"CallStack": "Version", "KeywordArgs": {}}')
+
+        while sZookeeper.ZookeeperConnection.get(ConnectionNode)[0] == b'{"CallStack": "Version", "KeywordArgs": {}}':
+            pass
+
+
+        return json.loads(sZookeeper.ZookeeperConnection.get(ConnectionNode)[0].decode())
+
+
+    # Start System #
+    mLogger.Log('Starting API Server')
+    mLogger.Log('')
+    mLogger.Log('---------------------------------------------------------------------------')
+    mLogger.Log('██████╗ ██████╗  █████╗ ██╗███╗   ██╗ ██████╗ ███████╗███╗   ██╗██╗██╗  ██╗')
+    mLogger.Log('██╔══██╗██╔══██╗██╔══██╗██║████╗  ██║██╔════╝ ██╔════╝████╗  ██║██║╚██╗██╔╝')
+    mLogger.Log('██████╔╝██████╔╝███████║██║██╔██╗ ██║██║  ███╗█████╗  ██╔██╗ ██║██║ ╚███╔╝ ')
+    mLogger.Log('██╔══██╗██╔══██╗██╔══██║██║██║╚██╗██║██║   ██║██╔══╝  ██║╚██╗██║██║ ██╔██╗ ')
+    mLogger.Log('██████╔╝██║  ██║██║  ██║██║██║ ╚████║╚██████╔╝███████╗██║ ╚████║██║██╔╝ ██╗')
+    mLogger.Log('╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝')
+    mLogger.Log('---------------------------------------------------------------------------')
+    mLogger.Log('')
+    mLogger.Log('    +-----------------------------------------------------------------+')
+    mLogger.Log(f'    |          Welcome To BrainGenix-APIServer Version {Version}          |')
+    mLogger.Log('    +-----------------------------------------------------------------+')
+    mLogger.Log('')
+
+
+# Instantiate Main #
+APIServerInstance = Main()
+
+# Start Uvicorn #
+if __name__ == '__main__':
+    uvicorn.run("APIServer:APIServerInstance", host="0.0.0.0", port=2001, log_level="info")
