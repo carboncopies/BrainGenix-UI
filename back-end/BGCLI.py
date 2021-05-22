453334bba6e9b4c232d53445922b3567afc81720
Thomas Liao <tliao@carboncopies.org>
2021-05-22 00:19:30 +0000

    Remove ZK BGCLI To Prepare For Socket Based CLI

--- back-end/BGCLI.py
+++ back-end/BGCLI.py
@@ -10,8 +10,8 @@ import time
 import uuid
 import atexit
 import json
+import requests
 
-from kazoo.client import KazooClient
 
 '''
 Name: ZKCLI-Client
@@ -34,21 +34,8 @@ class Client(): # Client For BrainGenix System #
         Initializes the Client, Authenticates, and waits until connection accepted.
         '''
 
-        # Connect To ZK #
-        self.ZookeeperConnection = KazooClient(hosts=ZKAddress)
-        self.ZookeeperConnection.start()
-        self.Host = ZKAddress
-
-
-        self.UUID = str(uuid.uuid1())
-
-        # Create A Connection zNode #
-        self.ConnectionNode = f'/BrainGenix/API/Connections/{self.UUID}'
-
-        self.ZookeeperConnection.create(self.ConnectionNode, ephemeral=True)
-
-        # Print Welcome Message #
-        print('Type "Help", "License", "Authors" or "Copyright" for more information.')
+        # Connect To HTTP API #
+        r = requests.post()
 
 
     def Main(self): # Main Loop #
@@ -58,61 +45,10 @@ class Client(): # Client For BrainGenix System #
         This contains the loop that gets the user's input and passes it on to the server.
         '''
 
-        # Loop For Connection #
-        while True:
-
-            # Get Command Input From User #
-            Command = input('root@BrainGenix:/# ')
-
-            if Command == '':
-                pass
-            elif Command == 'exit':
-                exit()
-            else:
-
-                # Format The Connection #
-                CommandParsed = Command.split(' ')
-
-                CommandPrefix = CommandParsed[0]
-
-                Arguments = {}
-                for Argument in CommandParsed[1:]:
-                    Value = Argument.split('=')[0]
-                    Value2 = Argument.split('=')[1]
-
-                    Arguments.update({Value : Value2})
-
-                # Compile Command Into Dictionary #
-                CommandDictionary = {}
-
-                CommandDictionary.update({'CallStack' : CommandPrefix})
-                CommandDictionary.update({'KeywordArgs' : Arguments})
-
-                # Encode As JSON #
-                JSONString = json.dumps(CommandDictionary)
+        
 
 
-                self.ZookeeperConnection.set(self.ConnectionNode, JSONString.encode())
-
-
-                # Block Until Command Finished #
-                while self.ZookeeperConnection.get(self.ConnectionNode)[0] == JSONString.encode():
-                    time.sleep(0.05)
-
-                # Handle Output #
-                CommandOutput = self.ZookeeperConnection.get(self.ConnectionNode)[0].decode()
-
-                if CommandOutput != '':
-                    print(CommandOutput)
-                else:
-                    pass
-
-# Set Exit Params #
-@atexit.register
-def Disconnect():
-    CLI.ZookeeperConnection.delete(CLI.ConnectionNode)
-    print('BGCLI Connection Destroyed Successfully')
 
 # Instantiate The Client #
-CLI = Client('10.0.4.3:2181')
+CLI = Client('10.1.4.2001')
 CLI.Main()
