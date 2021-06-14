70e421cb9f7ea21b7accd128e759f09199fcac7d
Allen Sulzen <allensulzen1@gmail.com>
2021-06-13 22:58:00 -0400

    added terminal and RAM line graph

--- front-end/js/scripts.js
+++ front-end/js/scripts.js
@@ -4,9 +4,9 @@ var ramChart = new Chart(
     {
         type: 'line',
         data: {
-            labels: ['', '', '', '', '', '', ''],
+            labels: ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
             datasets: [{
-                label: 'RAM Usage (%)',
+                label: '',
                 backgroundColor: 'rgba(55, 168, 67, .2)',
                 pointBackgroundColor: '#37a843',
                 borderColor: '#37a843',
@@ -15,7 +15,7 @@ var ramChart = new Chart(
                 pointRadius: 0,
                 cubicInterpolationMode: 'monotone',
                 pointHoverRadius: 2,
-                data: [0, 10, 5, 2, 20, 30, 45],
+                data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                 fill: {
                     target: 'origin',
                     below: 'rgba(55, 168, 67, .2)'
@@ -23,20 +23,39 @@ var ramChart = new Chart(
             }]
         },
         options: {
+            plugins: {
+                legend: {
+                    display: false
+                },
+            },
             scales: {
                 y: {
                     stacked: true,
                     grid: {
-                        color: '#fff'
+                        color: 'rgba(255, 255, 255, .2)'
                     },
-                    labels: {
-                        color: '#fff'
-                    }
+                    gridLines: {
+                        display: false
+                    },
+                    min: 0,
+                    max: 100
                 },
                 x: {
                     grid: {
-                        color: '#fff'
+                        color: 'transparent'
                     }
+                }
+            },
+            animation: {
+                duration: 0 // general animation time
+            },
+            hover: {
+                animationDuration: 0 // duration of animations when hovering an item
+            },
+            responsiveAnimationDuration: 0, // animation duration after a resize
+            elements: {
+                line: {
+                    tension: 0 // disables bezier curves
                 }
             }
         }
@@ -48,15 +67,54 @@ var cpuChart = new Chart(
     {
         type: 'line',
         data: {
-            labels: ['', '', '', '', '', '', ''],
+            labels: ['', '', '', '', '', '', '', '', '', ''],
             datasets: [{
+                max: 100,
                 label: 'CPU Usage (%)',
-                backgroundColor: '#37a843',
+                backgroundColor: 'rgba(55, 168, 67, .2)',
+                pointBackgroundColor: '#37a843',
                 borderColor: '#37a843',
-                data: [0, 10, 5, 2, 20, 30, 45],
+                borderWidth: 1,
+                color: '#fff',
+                pointRadius: 0,
+                // cubicInterpolationMode: 'monotone',
+                pointHoverRadius: 2,
+                data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                fill: {
+                    target: 'origin',
+                    below: 'rgba(55, 168, 67, .2)'
+                }
             }]
         },
-        options: {}
+        options: {
+            scales: {
+                y: {
+                    stacked: true,
+                    grid: {
+                        color: 'transparent'
+                    },
+                    min: 0,
+                    max: 100
+                },
+                x: {
+                    grid: {
+                        color: 'transparent'
+                    }
+                }
+            },
+            animation: {
+                duration: 0 // general animation time
+            },
+            hover: {
+                animationDuration: 0 // duration of animations when hovering an item
+            },
+            responsiveAnimationDuration: 0, // animation duration after a resize
+            elements: {
+                line: {
+                    tension: 0 // disables bezier curves
+                }
+            }
+        }
     }
 );
 
@@ -80,7 +138,12 @@ var fetchInt = setInterval(() => {
                 && currentFetch.Content 
                 && currentFetch.Content[n] 
                 && currentFetch.Content[n].RAMUsage) {
-                document.querySelector('#ram-usage span').innerText = currentFetch.Content[n].RAMUsage;
+                document.querySelector('span#ru-stats').innerText = currentFetch.Content[n].RAMPercent + '% | ' + currentFetch.Content[n].RAMUsage;
+                ramChart.data.datasets[0].data.push(currentFetch.Content[n].RAMPercent);
+                if (ramChart.data.datasets[0].data.length > 20) {
+                    ramChart.data.datasets[0].data.shift();
+                }
+                ramChart.update();
             }
         }
         if (!!nodeList) {
@@ -88,7 +151,53 @@ var fetchInt = setInterval(() => {
         }
     })
     .catch(function (error) {
-        // handle error
-        console.log(error);
+        clearInterval(fetchInt);
+        notify('The API server cannot be reached!', 'error');
     });
-}, 1000);
+}, 75);
+
+// notifications 
+function notify(message, status) {
+    let el = document.querySelector('div.notification'),
+        t = '<h2>Error</h2>',
+        b = '<div class="close-notification" onclick="notify();"><span class="iconify" data-icon="ion-close-circle-outline"></span></div>';
+    if (!message || message === 'quit') {
+        el.classList.remove('active'); 
+        return;
+    }
+    switch(status) {
+        case 'log':
+            t = '<h2 class="notify-log">Log</h2>';
+            break;
+        case 'error':
+            t = '<h2 class="notify-error">Error</h2>';
+            break;
+        case 'success':
+            t = '<h2 class="notify-success">Success!</h2>';
+            break;
+    }
+    el.innerHTML = b + t + '<p>' + message + '</p>';
+    el.classList.add('active');
+}
+
+document.querySelector('div.close-notification').addEventListener('click', () => { notify(); });
+
+// router and navigation 
+function navigate(page) {
+    let pgs = document.querySelectorAll('.pg');
+    pgs.forEach(pg => {
+        pg.classList.remove('active');
+    });
+    document.querySelector('#' + page).classList.add('active');
+    if (page === 'Terminal') {
+        // terminal 
+        var term = new Terminal();
+        var fitAddon = new FitAddon.FitAddon();
+        term.loadAddon(fitAddon);
+        term.open(document.getElementById('terminal'));
+        term.write('BrainGenix-UI $ ');
+        window.term = term;
+        window.fitAddon = fitAddon;
+        setTimeout(() => { fitAddon.fit(); }, 250);
+    }
+}
