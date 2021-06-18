4a2ffe7e22d004dd72edf49a98289d56781cea4a
J-French1 <85071286+J-French1@users.noreply.github.com>
2021-06-18 16:51:06 -0500

    Update scripts.js

--- front-end/js/scripts.js
+++ front-end/js/scripts.js
@@ -1,24 +1,24 @@
 // charts
 var ramChart = new Chart(
-    document.querySelector('div#ram-usage canvas.stat').getContext('2d'),
+    document.querySelector("div#ram-usage canvas.stat").getContext("2d"),
     {
-        type: 'line',
+        type: "line",
         data: {
-            labels: ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+            labels: ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
             datasets: [{
-                label: '',
-                backgroundColor: 'rgba(55, 168, 67, .2)',
-                pointBackgroundColor: '#37a843',
-                borderColor: '#37a843',
+                label: "",
+                backgroundColor: "rgba(55, 168, 67, .2)",
+                pointBackgroundColor: "#37a843",
+                borderColor: "#37a843",
                 borderWidth: 1,
-                color: '#fff',
+                color: "#fff",
                 pointRadius: 0,
-                cubicInterpolationMode: 'monotone',
+                cubicInterpolationMode: "monotone",
                 pointHoverRadius: 2,
                 data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                 fill: {
-                    target: 'origin',
-                    below: 'rgba(55, 168, 67, .2)'
+                    target: "origin",
+                    below: "rgba(55, 168, 67, .2)"
                 }
             }]
         },
@@ -32,7 +32,7 @@ var ramChart = new Chart(
                 y: {
                     stacked: true,
                     grid: {
-                        color: 'rgba(255, 255, 255, .2)'
+                        color: "rgba(255, 255, 255, .2)"
                     },
                     gridLines: {
                         display: false
@@ -42,7 +42,7 @@ var ramChart = new Chart(
                 },
                 x: {
                     grid: {
-                        color: 'transparent'
+                        color: "transparent"
                     }
                 }
             },
@@ -63,26 +63,26 @@ var ramChart = new Chart(
 );
 
 var cpuChart = new Chart(
-    document.querySelector('div#cpu canvas.stat').getContext('2d'),
+    document.querySelector("div#cpu canvas.stat").getContext("2d"),
     {
-        type: 'line',
+        type: "line",
         data: {
-            labels: ['', '', '', '', '', '', '', '', '', ''],
+            labels: ["", "", "", "", "", "", "", "", "", ""],
             datasets: [{
                 max: 100,
-                label: 'CPU Usage (%)',
-                backgroundColor: 'rgba(55, 168, 67, .2)',
-                pointBackgroundColor: '#37a843',
-                borderColor: '#37a843',
+                label: "CPU Usage (%)",
+                backgroundColor: "rgba(55, 168, 67, .2)",
+                pointBackgroundColor: "#37a843",
+                borderColor: "#37a843",
                 borderWidth: 1,
-                color: '#fff',
+                color: "#fff",
                 pointRadius: 0,
-                // cubicInterpolationMode: 'monotone',
+                // cubicInterpolationMode: "monotone",
                 pointHoverRadius: 2,
                 data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                 fill: {
-                    target: 'origin',
-                    below: 'rgba(55, 168, 67, .2)'
+                    target: "origin",
+                    below: "rgba(55, 168, 67, .2)"
                 }
             }]
         },
@@ -91,14 +91,14 @@ var cpuChart = new Chart(
                 y: {
                     stacked: true,
                     grid: {
-                        color: 'transparent'
+                        color: "transparent"
                     },
                     min: 0,
                     max: 100
                 },
                 x: {
                     grid: {
-                        color: 'transparent'
+                        color: "transparent"
                     }
                 }
             },
@@ -123,22 +123,22 @@ const fetchHistory = [];
 let currentFetch = {};
 
 var fetchInt = setInterval(() => {
-    axios.post('http://50.255.24.101:2001/', {
-        SysName: 'NES', 
-        CallStack:'LFTM.SystemTelemetryManager.mAPI_GetAllNodeStats',
+    axios.post("http://50.255.24.101:2001/", {
+        SysName: "NES", 
+        CallStack:"LFTM.SystemTelemetryManager.mAPI_GetAllNodeStats",
         KeywordArgs: {}
     }).then(function (response) {
         // handle success
         fetchHistory.push(response.data);
         currentFetch = response.data;
-        let nodeList = '';
+        let nodeList = "";
         for (var n in currentFetch.Content) {
-            nodeList += '<li>' + n + '</li>';
+            nodeList += "<li>" + n + "</li>";
             if (currentFetch 
                 && currentFetch.Content 
                 && currentFetch.Content[n] 
                 && currentFetch.Content[n].RAMUsage) {
-                document.querySelector('span#ru-stats').innerText = currentFetch.Content[n].RAMPercent + '% | ' + currentFetch.Content[n].RAMUsage;
+                document.querySelector("span#ru-stats").innerText = currentFetch.Content[n].RAMPercent + "% | " + currentFetch.Content[n].RAMUsage;
                 ramChart.data.datasets[0].data.push(currentFetch.Content[n].RAMPercent);
                 if (ramChart.data.datasets[0].data.length > 20) {
                     ramChart.data.datasets[0].data.shift();
@@ -147,18 +147,18 @@ var fetchInt = setInterval(() => {
             }
         }
         if (!!nodeList) {
-            document.querySelector('#node-list').innerHTML = nodeList;
+            document.querySelector("#node-list").innerHTML = nodeList;
         }
     })
     .catch(function (error) {
         clearInterval(fetchInt);
-        notify('The API server cannot be reached!', 'error');
+        notify("The API server cannot be reached!", "error");
     });
 }, 75);
 
 // notifications 
 function notify(message, status) {
-    let el = document.querySelector('div.notification'),
+    let el = document.querySelector("div.notification"),
         t = '<h2>Error</h2>',
         b = '<div class="close-notification" onclick="notify();"><span class="iconify" data-icon="ion-close-circle-outline"></span></div>';
     if (!message || message === 'quit') {
