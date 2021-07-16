b4b3c8d16159c89375357b1e82330f07dddb33a9
Thomas Liao <tliao@carboncopies.org>
2021-07-16 14:23:10 -0700

    Refactor BGUI To Use Modern NES Subsystems

--- back-end/Core/Management/Logger/CLAS.py
+++ back-end/Core/Management/Logger/CLAS.py
@@ -2,7 +2,8 @@
 ## This file is part of the BrainGenix Simulation System ##
 ###########################################################
 
-
+import datetime
+import pymysql
 
 '''
 Name: CLAS
@@ -13,50 +14,74 @@ Date-Created: 2021-04-25
 
 class CentralizedLoggerAggregationSystem(): # Define Class for CLAS #
 
-    def __init__(self, Logger): # Initialization Function #
+    def __init__(self, Logger:object, SystemConfiguration:dict): # Initialization Function #
 
         # Create Local Pointers To Logger Object #
         self.Logger = Logger
+        self.SystemConfiguration = SystemConfiguration
 
         # Print "Welcome" Message #
-        self.Logger.Log('Initializing Centralized Logger Aggregation System')
+        self.Logger.Log('Initializing Centralized Logger Aggregation System', 4)
+
+
+        # Read Log Retention Second Number #
+        self.SecondsToKeepLogs = int(SystemConfiguration['SecondsToKeepLogs'])
+
+
+        # Read MYSQL Information From System Configuration Dictionary #
+        self.Logger.Log('Reading CLAS Configuration Parameters From Local Configuration File', 2)
+        DBUsername = str(SystemConfiguration.get('DatabaseUsername'))
+        DBPassword = str(SystemConfiguration.get('DatabasePassword'))
+        DBHost = str(SystemConfiguration.get('DatabaseHost'))
+        DBDatabaseName = str(SystemConfiguration.get('DatabaseName'))
+        self.Logger.Log('Read CLAS Configuration Parameters', 1)
+
+
+        # Connect To DB Server #
+        self.Logger.Log('Creating CLAS PymySQL Instance, Connecting To Database Server', 2)
+        self.DatabaseConnection = pymysql.connect(
+            host = DBHost,
+            user = DBUsername,
+            password = DBPassword,
+            db = DBDatabaseName
+        )
+        self.Logger.Log('Created CLAS PymySQL Instance', 1)
+
+        # Create Database Cursor #
+        self.Logger.Log('Creating CLAS Daemon Cursor', 2)
+        self.LoggerCursor = self.DatabaseConnection.cursor()
+        self.Logger.Log('Created CLAS Cursor', 1)
+
+        # Create Help Strings #
+        self.Logger.Log('Setting CLAS mAPI Help Strings', 2)
+        self.mAPI_ReadLog_Help = ''
+        self.Logger.Log('Set CLAS mAPI Help Strings', 1)
+
 
         # Finalize Init #
-        self.Logger.Log('Finished CLAS Initialization')
+        self.Logger.Log('Finished CLAS Initialization', 3)
 
 
 
-    def ReadLog(self, Lines=500): # Read Lines From The Log, Default=500Lines #
+    def mAPI_ReadLog(self, APIArgs): # Read Lines From The Log, Default=500Lines #
+
+        # Get Lines Param From APIArgs Dict #
+        NumLines = APIArgs['Lines']
 
         # Get Log Text #
-        LogEntries = self.Logger.PullLog(Lines)
+        PullStatement= ("SELECT * FROM log LIMIT %d" % int(NumLines))
+        self.LoggerCursor.execute(PullStatement)
+
+        LogEntries = self.LoggerCursor.fetchall()
+        CommandOutput = []
+
+        # Convert Datetime Objects To Unix Epoch Time (UTC) #
+        for LogEntry in LogEntries:
+
+            OutputEntry = list(LogEntry)
+            OutputEntry[2] = OutputEntry[2].timestamp()
+
+            CommandOutput.append(OutputEntry)
 
         # Return Log Text #
-        return LogEntries
-
-
-    def ProcessLog(self, Lines=500): # Read And Filter Lines From The Log # 
-
-        # Get Log Text #
-        LogEntries = self.Logger.PullLog(Lines)
-
-        # Identify Nodes #
-        Nodes = []
-
-        for Item in LogEntries:
-            if Item[6] not in Nodes:
-                Nodes.append(Item[6])
-
-        # Generate Output Format #
-        Output = {}
-
-        for NodeName in Nodes:
-            Output.update({NodeName : []})
-
-        # Sort Into List #
-        for LogItem in LogEntries:
-            LineNodeName = LogItem[6]
-            Output[LineNodeName].append(LogItem)
-
-        # Return Output #
-        return Output
+        return CommandOutput
