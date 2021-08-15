7599af348d7d14d531506ec7899704cdcdbbea9a
joshua <jfrench@carboncopies.org>
2021-08-15 10:20:26 -0500

    fix whitespaces

--- back-end/Core/Internode/Database/DatabaseInterface.py
+++ back-end/Core/Internode/Database/DatabaseInterface.py
@@ -172,7 +172,7 @@ class DBInterface(): # Interface to MySQL database #
     def GetSynapsesOfNodes(self, NodeIds:list): # gets all synapses with equations for a list of neuronId#
 
         Placeholders = ','.join(NodeIds)
-        
-        SynapsesOfNeurons= self.DatabaseCursor.execute("SELECT synapseId, xCoord, yCoord, zCoord, neuronId, equationId, equationText from bgdb.synapse s inner join bgdb.equation e on e.equationId = s.equationId WHERE neuronId in (%s)" % Placeholders) 
+
+        SynapsesOfNeurons= self.DatabaseCursor.execute("SELECT synapseId, xCoord, yCoord, zCoord, neuronId, equationId, equationText from bgdb.synapse s inner join bgdb.equation e on e.equationId = s.equationId WHERE neuronId in (%s)" % Placeholders)
 
         return SynapsesOfNeurons
